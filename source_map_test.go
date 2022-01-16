5f2490c57d045d6715ffd9ee55cb81d28e0df7d8
Aleksandr Shilov <alex.shilov.by@gmail.com>
2022-01-16 19:25:52 +0300

    Add missing test
    Signed-off-by: Aleksandr Shilov <alex.shilov.by@gmail.com>

---
+++ source_map_test.go
@@ -0,0 +1,57 @@
+package russian_calendar
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSourceMap_Exists(t *testing.T) {
+	type args struct {
+		desc  string
+		input time.Time
+		want  bool
+		err   error
+	}
+
+	source := &SourceMap{
+		2000: {
+			1: {
+				5: true,
+			},
+		},
+	}
+
+	tests := []args{
+		{
+			desc:  "day matched",
+			input: Date(2000, 1, 5),
+			want:  true,
+		}, {
+			desc:  "month matched",
+			input: Date(2000, 1, 1),
+			want:  false,
+		}, {
+			desc:  "month mismatched",
+			input: Date(2000, 2, 1),
+			err:   ErrOutOfRange,
+		}, {
+			desc:  "year mismatched",
+			input: Date(1999, 1, 5),
+			err:   ErrOutOfRange,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			got, err := source.Exists(tt.input)
+
+			require.Equal(t, got, tt.want)
+
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+			}
+		})
+	}
+}
